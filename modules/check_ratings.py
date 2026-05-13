31998bb24ae08d2afba9706604d561f8073a618a
Joanna <joanna.siemanska@gmail.com>
2026-05-13 23:31:04 +0200

    wersja z best

--- modules/check_ratings.py
+++ modules/check_ratings.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import numpy as np
+import os
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 def check_ratings(ratings_file, mode):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(current_dir) 
+    test_path = os.path.join(project_root, "data", "ratings_test.csv")
+    valid_path = os.path.join(project_root, "data", "ratings_valid.csv")
+
     df = pd.read_csv(ratings_file)
+    
     if mode == "predict":
-        #df2 = pd.read_csv("C:\\Users\\d-het\\MCaDR\\data\\ratings_test.csv")
-        df2 = pd.read_csv("C:\\Users\\joann\\Desktop\\MoCaDR\\Recomendation_system-main\\Recomendation_system-main\\data\\ratings_test.csv")
+        df2 = pd.read_csv(test_path)
     else:
-        #df2 = pd.read_csv("C:\\Users\\d-het\\MCaDR\\data\\ratings_valid.csv")
-        df2 = pd.read_csv("C:\\Users\\joann\\Desktop\\MoCaDR\\Recomendation_system-main\\Recomendation_system-main\\data\\ratings_valid.csv")
+        df2 = pd.read_csv(valid_path)
 
     rmse = np.sqrt(mean_squared_error(df['rating'], df2['rating']))
     mae = mean_absolute_error(df['rating'], df2['rating'])
     r2 = r2_score(df2['rating'], df['rating'])
-    return rmse, mae, r2
-
-
     
+    return rmse, mae, r2
